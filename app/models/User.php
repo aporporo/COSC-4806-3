c02f69c748f99ca963a1654df025ee423307e25e
aporporo <117672857+aporporo@users.noreply.github.com>
2024-06-06 18:46:14 +0000

    added functions to create users in user.php

--- app/models/User.php
+++ app/models/User.php
@@ -47,4 +47,27 @@ class User {
 		}
     }
 
+  //create user and insert into user table
+  public function create_user ($username, $password) {
+    $db = db_connect();
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $statement = $db->prepare("INSERT INTO users (username, password) VALUES ('$username', '$hash')");
+    $statement->execute();
+
+  }
+
+  //check if username exists in user table
+  public function check_user_exists ($username) {
+    $db = db_connect();
+    $statement = $db->prepare("SELECT * FROM users WHERE username = '$username'");
+    $statement->execute();
+    $rows = $statement->fetch(PDO::FETCH_ASSOC);
+    if (isset($rows['username'])) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
 }
