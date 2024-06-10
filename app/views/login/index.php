5c9bfd51daa89640afe0d1222531de7b8a32e974
aporporo <117672857+aporporo@users.noreply.github.com>
2024-06-10 17:31:36 +0000

    update css formating on login page

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -8,30 +8,48 @@
         </div>
     </div>
 
-<div class="row">
-    <div class="col-sm-auto">
-		<form action="/login/verify" method="post" >
-		<fieldset>
-			<div class="form-group">
-				<label for="username">Username</label>
-				<input required type="text" class="form-control" name="username">
+	<section class="vh-50">
+		<div class="container py-2 h-100" >
+			<div class="row d-flex justify-content-center align-items-center h-100">
+				<div class="col-md-8">
+					<div style="border-radius: 1rem; background-color: #696969;">
+						<div class="card-body p-5 text-center">
+							<?php if (isset($_SESSION['failedAuth']) && $_SESSION['failedAuth'] >= 3): ?>
+									<div class="alert alert-danger" role="alert">
+											<?php echo "Exceeded failed login attempts, please wait 60 seconds"; ?>
+
+									</div>
+							<?php endif ?>
+							<h1 class="mb-4" style="color: white">Sign in</h1>
+
+							
+    
+							<form action="/login/verify" method="post" >
+							<fieldset>
+							<div data-mdb-input-init class="form-outline mb-3">
+				
+								<input required type="text" class="form-control form-control-lg" name="username" placeholder="Username">
+							</div>
+							<div data-mdb-input-init class="form-outline mb-3">
+				
+								<input required type="password" class="form-control form-control-lg" name="password" placeholder="Password">
+							</div>
+            
+		    
+							<button type="submit" class="btn btn-primary btn-lg" >Login</button>
+								<hr class="my-3">
+								
+								<p><a href="/create" class="text-white">Register Here</a></p>
+						</fieldset>
+						</form> 
+							
+						
+					</div>
+					
+					</div>
+				</div>
 			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input required type="password" class="form-control" name="password">
-			</div>
-            <br>
-		    <button type="submit" class="btn btn-primary">Login</button>
-		</fieldset>
-		</form> 
-			<p><a href="/create">Register</a></p>
-	</div>
-	<div class="col-sm-6">
-			<br>
-	<?php if (isset($_SESSION['failedAuth']) && $_SESSION['failedAuth'] >= 3): ?>
-			<div class="alert alert-danger" role="alert">
-					<?php echo "Exceeded failed login attempts, please wait 60 seconds"; ?>
-			</div>
-	<?php endif ?>
-</div>
+		</div>
+	</section>
+	
     <?php require_once 'app/views/templates/footer.php' ?>
